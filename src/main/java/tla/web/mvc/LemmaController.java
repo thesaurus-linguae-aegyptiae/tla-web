2da82025a7759342482346332af21e3ec760c5fc
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-04-22 12:48:44 +0200

    add request interceptor injecting common header metadata

--- src/main/java/tla/web/mvc/LemmaController.java
+++ src/main/java/tla/web/mvc/LemmaController.java
@@ -2,7 +2,6 @@ package tla.web.mvc;
 
 import tla.web.model.Lemma;
 import tla.web.model.ObjectDetails;
-import tla.web.mvc.MvcConfig.TlaPageHeader;
 import tla.web.service.LemmaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +20,12 @@ public class LemmaController {
     @Autowired
     private LemmaService lemmaService;
 
-    @Autowired
-    private TlaPageHeader pageHeader;
-
     @RequestMapping(value = "/lemma/{id}", method = RequestMethod.GET)
     public String getLemmaDetails(Model model, @PathVariable String id) {
         log.debug("Compile lemma detail view data for lemma {}", id);
         ObjectDetails<Lemma> container = lemmaService.getLemma(id);
         Lemma lemma = container.getObject();
         model.addAttribute("obj", lemma);
-        model.addAttribute("env", pageHeader);
         model.addAttribute("hieroglyphs", lemmaService.extractHieroglyphs(lemma));
         model.addAttribute("bibliography", lemmaService.extractBibliography(lemma));
         model.addAttribute("related", container.getRelatedObjects());
--- src/main/java/tla/web/mvc/MvcConfig.java
+++ src/main/java/tla/web/mvc/MvcConfig.java
@@ -1,15 +1,25 @@
 package tla.web.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import lombok.extern.slf4j.Slf4j;
 import tla.web.config.ApplicationProperties;
 
+@Slf4j
 @Configuration
 @Import(ApplicationProperties.class)
-public class MvcConfig {
+public class MvcConfig  implements WebMvcConfigurer {
 
     @Autowired
     private ApplicationProperties applicationProperties;
@@ -17,6 +27,21 @@ public class MvcConfig {
     @Bean
     public TlaPageHeader tlaPageHeader() {
         return new TlaPageHeader(applicationProperties);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("registering MVC handler interceptor");
+        registry.addInterceptor(
+            new RequestMappingInterceptor(applicationProperties)
+        );
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/resources/*")
+            .addResourceLocations("/resources/");
     }
 
     public static class TlaPageHeader {
@@ -63,6 +88,37 @@ public class MvcConfig {
 
         public String getAppName() {
             return props.getName();
+        }
+    }
+
+    public class RequestMappingInterceptor extends HandlerInterceptorAdapter {
+
+        private TlaPageHeader headerMetadata;
+
+        public RequestMappingInterceptor(ApplicationProperties properties) {
+            log.info(
+                "instantiating MVC interceptor with {} application properties",
+                properties != null ? "non-null" : "unavailable"
+            );
+            this.headerMetadata = new TlaPageHeader(properties);
+        }
+
+        @Override
+        public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView
+        ) throws Exception {
+            if (modelAndView != null) {
+                log.info(
+                    "intercepting {} request to {} - extending view model",
+                    request.getMethod(),
+                    request.getRequestURI()
+                );
+                modelAndView.getModelMap().addAttribute("env", headerMetadata);
+            }
+            super.postHandle(request, response, handler, modelAndView);
         }
     }
 }
