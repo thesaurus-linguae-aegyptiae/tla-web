a595f19c935ef7274344adea2a47f3cb7834f8fc
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-05-11 19:49:50 +0200

    use configured word classes in search form

--- src/main/java/tla/web/mvc/SearchController.java
+++ src/main/java/tla/web/mvc/SearchController.java
@@ -1,5 +1,9 @@
 package tla.web.mvc;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import tla.domain.command.LemmaSearch;
 import tla.domain.model.Language;
 import tla.domain.model.Script;
+import tla.web.config.SearchProperties;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
+
+    @Autowired
+    private SearchProperties searchConfig;
 
     @ModelAttribute("allScripts")
     public Script[] getAllScripts() {
@@ -24,7 +32,12 @@ public class SearchController {
         return LemmaController.SEARCHABLE_TRANSLATION_LANGUAGES;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ModelAttribute("wordClasses")
+    public Map<String, List<String>> getWordClasses() {
+        return searchConfig.getWordClasses();
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String mainSearchPage(Model model) {
         model.addAttribute("lemmaSearchForm", new LemmaSearch());
         return "search";
