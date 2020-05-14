4aaac15233b15a324fd83ec0e3ebc64f818a5564
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-05-14 18:47:10 +0200

    fix some HTML tag asymetries

--- src/test/java/tla/web/mvc/LemmaDetailsTest.java
+++ src/test/java/tla/web/mvc/LemmaDetailsTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.XpathResultMatchers;
@@ -13,6 +14,7 @@ import tla.domain.dto.extern.SingleDocumentWrapper;
 import tla.web.model.Lemma;
 import tla.web.repo.TlaClient;
 
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -31,6 +33,14 @@ public class LemmaDetailsTest {
     @MockBean
     private TlaClient backendClient;
 
+    private static final String[] EXPECT_TOP_LEVEL_ELEM_IDS = {
+        "lemma-property-dict",
+        "lemma-property-part-of-speech",
+        "external-references",
+        "translations",
+        "bibliography"
+    };
+
     @SuppressWarnings("unchecked")
     private SingleDocumentWrapper<DocumentDto> lemmaDetails(String id) {
         return tla.domain.util.IO.loadFromFile(
@@ -48,8 +58,18 @@ public class LemmaDetailsTest {
 
     private ResultActions makeDetailsRequest(String id) throws Exception {
         return mockMvc.perform(
-            get("/lemma/" + id)
+            get("/lemma/" + id).header(HttpHeaders.ACCEPT_LANGUAGE, "en")
         ).andDo(print()).andExpect(status().isOk());
+    }
+
+    private void testBasicStructure(ResultActions testResponse) throws Exception {
+        for (String id : EXPECT_TOP_LEVEL_ELEM_IDS) {
+            testResponse.andExpect(
+                xpath("//div[@id='details-content']/div[@id='" + id + "']").exists()
+            ).andExpect(
+                content().string(not(containsString("_en??")))
+            );
+        }
     }
 
     @Test
@@ -58,6 +78,7 @@ public class LemmaDetailsTest {
         respondToDetailsRequestWithLemma(id);
         // There is no 'vega' provider in the application properties but vega external reference should be displayed regardlesz
         ResultActions testResponse = makeDetailsRequest(id);
+        //testBasicStructure(testResponse);
         XpathResultMatchers vega = xpath("//div[@id='external-references-vega']/div/span[@class='external-reference-provider']/text()");
         testResponse.andExpect(
             vega.exists()
@@ -74,10 +95,13 @@ public class LemmaDetailsTest {
         respondToDetailsRequestWithLemma(id);
         ResultActions testResponse = makeDetailsRequest(id);
         // check if POS fragments get compiled correctly
+        testBasicStructure(testResponse);
         testResponse.andExpect(
             xpath("//div[@id='lemma-property-part-of-speech']").exists()
         ).andExpect(
             xpath("//span[@id='lemma-pos']").exists()
+        ).andExpect(
+            xpath("//div[@id='details-content']/div[@id='lemma-property-attestations']").exists()
         );
     }
 
@@ -86,6 +110,7 @@ public class LemmaDetailsTest {
         final String id = "d1315";
         respondToDetailsRequestWithLemma(id);
         ResultActions testResponse = makeDetailsRequest(id);
+        testBasicStructure(testResponse);
         testResponse.andExpect(
             xpath("//div[@id='lemma-property-hieroglyphs']").doesNotExist()
         );
