c74eae93ba7146820a4ec78cee469c772418712a
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-05-20 13:37:16 +0200

    render pagination into template

--- src/main/java/tla/web/model/ui/Pagination.java
+++ src/main/java/tla/web/model/ui/Pagination.java
@@ -1,5 +1,8 @@
 package tla.web.model.ui;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -13,12 +16,15 @@ import tla.domain.dto.extern.PageInfo;
 @Getter
 public class Pagination {
 
+    public final static int OMISSION_THRESHOLD = 2;
+
     private long fromResult;
     private long toResult;
     private long totalResults;
 
     private List<Integer> pages;
     private int currentPage;
+    private int lastPage;
 
     /**
      * Takes the pagination info from DTO and turns it into something we can
@@ -30,17 +36,56 @@ public class Pagination {
             this.totalResults = dtoPage.getTotalElements();
             this.fromResult = (this.currentPage - 1) * dtoPage.getSize() + 1;
             this.toResult = (this.currentPage - 1) * dtoPage.getSize() + dtoPage.getNumberOfElements();
-            this.pages = new LinkedList<Integer>();
-            int lastPage = Math.max(dtoPage.getTotalPages(), 1);
-            IntStream.rangeClosed(1, lastPage).forEach(
-                p -> {
-                    this.pages.add(Integer.valueOf(p));
+            this.lastPage = Math.max(dtoPage.getTotalPages(), 1);
+            this.pages = this.makePageLinks();
+        } else {
+            this.currentPage = 1;
+            this.pages = List.of(this.currentPage);
+        }
+    }
+
+    /**
+     * Returns distance to first, last, or current page, depending on what is
+     * closest.
+     * @param page
+     * @return
+     */
+    protected int distanceToRequiredPage(int page) {
+        return Arrays.asList(
+            1,
+            this.currentPage,
+            this.lastPage
+        ).stream().map(
+            requiredPage -> Math.abs(requiredPage - page)
+        ).reduce(
+            Math::min
+        ).get();
+    }
+
+    /**
+     * Creates a sequence of <code>Integer</code> values representing the pages
+     * covered by this pagination object. <code>null</code> values represent
+     * ellipsis placeholders (n pages omitted).
+     */
+    protected List<Integer> makePageLinks() {
+        List<Integer> pages = new LinkedList<Integer>();
+        List<Integer> omitted = new LinkedList<Integer>();
+        IntStream.rangeClosed(1, this.lastPage).forEach(
+            p -> {
+                if (this.distanceToRequiredPage(p) > OMISSION_THRESHOLD) {
+                    omitted.add(p);
+                } else {
+                    if (omitted.size() > 1) {
+                        pages.add(null);
+                    } else {
+                        pages.addAll(omitted);
+                    }
+                    omitted.clear();
+                    pages.add(p);
                 }
-            );
-            } else {
-                this.currentPage = 1;
-                this.pages = List.of(this.currentPage);
             }
+        );
+        return pages;
     }
 
 }
--- src/test/java/tla/web/model/ui/PaginationTest.java
+++ src/test/java/tla/web/model/ui/PaginationTest.java
@@ -2,11 +2,23 @@ package tla.web.model.ui;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
 import tla.domain.dto.extern.PageInfo;
 
 public class PaginationTest {
+
+    private PageInfo makePageInfo(int pageSize, int totalElements, int totalPages, int currentPage) {
+        PageInfo dto = new PageInfo();
+        dto.setNumber(currentPage - 1);
+        dto.setSize(pageSize);
+        dto.setTotalElements(totalElements);
+        dto.setTotalPages(totalPages);
+        dto.setNumberOfElements(20);
+        return dto;
+    }
 
     @Test
     void nullInput() {
@@ -18,21 +30,36 @@ public class PaginationTest {
 
     @Test
     void firstPage() {
-        PageInfo dto = new PageInfo();
-        dto.setNumber(0);
-        dto.setSize(20);
-        dto.setTotalElements(199);
-        dto.setTotalPages(10);
-        dto.setNumberOfElements(20);
+        PageInfo dto = makePageInfo(20, 199, 10, 1);
         Pagination p = new Pagination(dto);
         assertAll("test UI pagination representation object for first page",
             () -> assertEquals(1, p.getCurrentPage(), "first page"),
-            () -> assertEquals(10, p.getPages().size(), "number pages"),
+            () -> assertEquals(0, dto.getNumber(), "pages in DTO start at 0"),
+            () -> assertEquals(7, p.getPages().size(), String.format("number pages. actual pages: %s", p.getPages())),
             () -> assertEquals(199, p.getTotalResults(), "total results"),
             () -> assertEquals(1, p.getFromResult(), "from result #"),
             () -> assertEquals(20, p.getToResult(), "to result #")
         );
+    }
 
+    @Test
+    void pageLinks() {
+        Pagination p = new Pagination(makePageInfo(20, 199, 14, 7));
+        assertAll("test page to required page distance function",
+            () -> assertEquals(2, p.distanceToRequiredPage(3), "page 3"),
+            () -> assertEquals(2, p.distanceToRequiredPage(9), "page 9"),
+            () -> assertEquals(3, p.distanceToRequiredPage(4), "page 4")
+        );
+        assertAll("test page range omission",
+            () -> assertEquals(13, p.getPages().size(), "1 page should be omitted"),
+            () -> assertEquals(
+                1,
+                Collections.frequency(p.getPages(), null),
+                String.format("expect 1 omission range but got: %s", p.getPages())
+            ),
+            () -> assertEquals(null, p.getPages().get(9), "10th & 11th page omitted"),
+            () -> assertEquals(12, p.getPages().get(10), "picking up after omission at 12th page")
+        );
     }
     
 }
