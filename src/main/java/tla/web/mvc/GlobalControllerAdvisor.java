00d3de02dfc728f328829e7c3181d74c740bb03b
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2020-06-01 18:09:36 +0200

    better to have only one @controlleradvice class

--- src/main/java/tla/web/mvc/GlobalControllerAdvisor.java
+++ src/main/java/tla/web/mvc/GlobalControllerAdvisor.java
@@ -1,31 +1,45 @@
 package tla.web.mvc;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 import tla.error.ObjectNotFoundException;
+import tla.web.config.ApplicationProperties;
 import tla.web.model.ui.BreadCrumb;
 
 @Controller
 @ControllerAdvice
-public class ExceptionController extends DefaultHandlerExceptionResolver {
+public class GlobalControllerAdvisor extends DefaultHandlerExceptionResolver {
 
-    @Autowired
-    private MvcConfig mvcConfig;
+    private ApplicationProperties applicationProperties;
+
+    public GlobalControllerAdvisor(ApplicationProperties properties) {
+        this.applicationProperties = properties;
+    }
+
+    @ModelAttribute ("env")
+    public Map<String, String> appVars() {
+        return Map.of(
+            "baseUrl", applicationProperties.getBaseUrl(),
+            "appName", applicationProperties.getName()
+        );
+    }
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ModelAndView handleObjectNotFound(ObjectNotFoundException e, HttpServletRequest request, Model model) {
-        model.addAttribute("env", mvcConfig.appVars());
+        model.addAttribute("env", appVars());
+        model.addAttribute("name", e.getClass().getSimpleName());
         model.addAttribute("code", 404);
         model.addAttribute("msg", e.getMessage());
         model.addAttribute("url", request.getRequestURI());
@@ -36,7 +50,7 @@ public class ExceptionController extends DefaultHandlerExceptionResolver {
                 BreadCrumb.of("/search", "menu_global_search")
             )
         );
-        return new ModelAndView("error/404", model.asMap(), HttpStatus.OK);
+        return new ModelAndView("error/404", model.asMap(), HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/tla/web/mvc/MvcConfig.java
+++ src/main/java/tla/web/mvc/MvcConfig.java
@@ -1,13 +1,9 @@
 package tla.web.mvc;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,18 +13,12 @@ import tla.web.model.mappings.LanguageFromStringConverter;
 import tla.web.model.mappings.ScriptFromStringConverter;
 
 @Configuration
-@ControllerAdvice
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Bean
     @Autowired
-    private ApplicationProperties applicationProperties;
-
-    @ModelAttribute("env")
-    public Map<String, String> appVars() {
-        return Map.of(
-            "baseUrl", applicationProperties.getBaseUrl(),
-            "appName", applicationProperties.getName()
-        );
+    public GlobalControllerAdvisor globalAdvisoryController(ApplicationProperties applicationProperties) {
+        return new GlobalControllerAdvisor(applicationProperties);
     }
 
     @Bean
