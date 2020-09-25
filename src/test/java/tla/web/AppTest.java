7351c16ae68ad9c257892690b30ce89b9deff698
Markus Schnöpf <schnoepf@bbaw.de>
2020-09-25 15:19:16 +0200

    insert new line. Why? Because I can

--- src/test/java/tla/web/AppTest.java
+++ src/test/java/tla/web/AppTest.java
@@ -1,5 +1,6 @@
 package tla.web;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
